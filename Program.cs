4f2f4a200527c7b4a11e86fdd7f0941e9bad3248
Yasin Çevik <yasincevik21@hotmail.com>
2020-05-22 18:18:08 +0300

    ODEVV

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YSNODEVVİZE
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap[] KitapDizi = new Kitap[0];
+            {
+                try
+                {
+                    Console.Write("Kitap,Yazar,Basım Yılı eklemek için 1'e, Kitap,Yazar,Basım Yılını görmek için 2'ye basın: ");
+                    int secim = int.Parse(Console.ReadLine());
+                    if (secim == 1)
+
+                    {
+
+                        Console.WriteLine("Kitap ekleme adeti?");
+                        byte kitapSecim = byte.Parse(Console.ReadLine());
+                        KitapDizi = new Kitap[kitapSecim];
+                        for (int i = 0; i < kitapSecim; i++)
+                        {
+                            Kitap kitap = new Kitap();
+                            Console.Write($"{i + 1}. Kitabınızın adı?: ");
+                            kitap.KitapTitle = Console.ReadLine();
+                            Console.Write($"{i + 1}. Kitabın yazarı?: ");
+                            kitap.KitapYazar = Console.ReadLine();
+                            Console.Write($"{i + 1}. Kitabın basım tarihi?: ");
+                            kitap.KitapTarih = DateTime.Parse(Console.ReadLine());
+                            while (kitap.KitapTarih.Year > 2020)
+                            {
+                                Console.Write($"{i + 1}. Kitabın basım yılını 2020'den küçük giriniz!");
+                                Console.Write($"{i + 1}. Kitabın basım tarihini giriniz: ");
+                                kitap.KitapTarih = DateTime.Parse(Console.ReadLine());
+                            }
+                            Console.Write($"{i + 1}. Kitabınızın türü?: ");
+                            kitap.KitapTur = Console.ReadLine();
+                            Console.WriteLine("Eklendi");
+                            KitapDizi[i] = kitap;
+                            Kitap.dosyaYaz(kitap.KitapTitle, kitap.KitapYazar, kitap.KitapTarih, kitap.KitapTur);
+                        }
+
+                        if (secim == 2)
+                        {
+                            Kitap.dosyaOku();
+
+
+                            Console.WriteLine("TAnımlanandan farklı bir değer girdiniz. Lütfen tekrar deneyin.");
+                        }
+
+
+                    }
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Boş değer.");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("HATA");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Size belirtilen değerin dışına çıktınız.");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ kitapozellik.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace YSNODEVVİZE
+{
+
+    public class Kitap
+    {
+        public Kitap()
+        {
+
+        }
+
+        public Kitap(string kitabinbasligi, string kitabinyazari, string kitapturu, DateTime kitabintarihi)
+        {
+            this.KitapTitle = kitabinbasligi;
+            this.KitapYazar = kitabinyazari;
+            this.KitapTur = kitapturu;
+            this.KitapTarih = kitabintarihi;
+        }
+        public string KitapTitle { get; set; }
+        public string KitapYazar { get; set; }
+        public string KitapTur { get; set; }
+        public DateTime KitapTarih { get; set; }
+        public string BilgileriGetir() => $"KitabınAdı:{this.KitapTitle}\nYazarınAdı:{this.KitapYazar}\nKitabınTürü:{this.KitapTur}\nKitabınTarihi:{this.KitapTarih}";
+
+
+        private static string dosyaYol = AppDomain.CurrentDomain.BaseDirectory + "Kitaplar.txt";
+        public static void dosyaOku()
+        {
+            if (File.Exists(dosyaYol))
+            {
+                Console.WriteLine("Kitaplar listesi yükleniyor");
+                string bilgileriOku = File.ReadAllText(dosyaYol);
+                Console.WriteLine(bilgileriOku);
+            }
+            else
+                Console.WriteLine("Herhangi bir kitap bilgisi eklenmemiş.");
+        }
+
+        public static void dosyaYaz(string kitabinbasligi, string kitabinyazari, DateTime kitabintarihi, string kitapturu)
+        {
+            string KitapBilgi = "Kitap adı: " + kitabinbasligi + "\nKitap Yazarı: " + kitabinyazari + "\nKitap Türü: " + kitapturu + "\nKitap Basım Tarihi: " + kitabintarihi;
+            File.AppendAllText(dosyaYol, KitapBilgi + Environment.NewLine);
+        }
+    }
+}
